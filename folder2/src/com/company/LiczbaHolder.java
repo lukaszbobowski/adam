6cd81d22e6a1e754885db8342a2fec2d73f10b73
lukasz <lu.bobowski@gmail.com>
2023-09-12 13:15:28 +0200

    komit

---
+++ folder2/src/com/company/LiczbaHolder.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class LiczbaHolder {
+    int a = 0;
+    public LiczbaHolder(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
