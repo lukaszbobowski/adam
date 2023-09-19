0abc98ffb398f0788201f1a63c1ba11a3f63c6f3
lukaszbobowski <lu.bobowski@gmail.com>
2023-09-19 13:25:23 +0200

    fajnie

---
+++ rzecz/src/com/company/Compare.java
@@ -0,0 +1,42 @@
+package com.company;
+
+public class Compare {
+    public Compare() {
+
+    }
+    public void compareValues(int a, int b) {
+        if(a > b) {
+            System.out.println(a + " jest wieksze od " + b);
+        } else if(a == b) {
+            System.out.println(a + " jest rowne " + b);
+        } else System.out.println(a + " jest mniejsze od " + b);
+    }
+    public void compareValues(float a, float b) {
+        if(a > b) {
+            System.out.println(a + " jest wieksze od " + b);
+        } else if(a == b) {
+            System.out.println(a + " jest rowne " + b);
+        } else System.out.println(a + " jest mniejsze od " + b);
+    }
+    public void compareValues(double a, double b) {
+        if(a > b) {
+            System.out.println(a + " jest wieksze od " + b);
+        } else if(a == b) {
+            System.out.println(a + " jest rowne " + b);
+        } else System.out.println(a + " jest mniejsze od " + b);
+    }
+    public void compareValues(byte a, byte b) {
+        if(a > b) {
+            System.out.println(a + " jest wieksze od " + b);
+        } else if(a == b) {
+            System.out.println(a + " jest rowne " + b);
+        } else System.out.println(a + " jest mniejsze od " + b);
+    }
+    public void compareValues(String a, String b) {
+        if(a.compareTo(b) > 0) {
+            System.out.println(a + " jest wieksze od " + b);
+        } else if(a.compareTo(b) == 0) {
+            System.out.println(a + " jest rowne " + b);
+        } else System.out.println(a + " jest mniejsze od " + b);
+    }
+}
--- rzecz/src/com/company/Main.java
+++ rzecz/src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        LiczbaHolder lh1 = new LiczbaHolder();
+       /* LiczbaHolder lh1 = new LiczbaHolder();
         LiczbaHolder lh2 = new LiczbaHolder();
         System.out.println(lh1.getA());
         lh1.setA(10);
@@ -17,6 +17,11 @@ public class Main {
         GenericHolder oh1 = new GenericHolder(123);
         System.out.println(oh1.getA());
         gh1.setA(333);
-        System.out.println(oh1.getA());
+        System.out.println(oh1.getA()); */
+
+        Compare a = new Compare();
+        a.compareValues("aaa", "aa");
+        com
+
     }
 }
